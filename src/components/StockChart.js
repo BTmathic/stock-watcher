ed8a90a19b3c951aa7e0fd7eb77c3a5e32c34b43
Alexander Molnar <mathic@gmail.com>
2018-10-02 21:01:02 -0400

    Stock data now renders on the chart and updates with user interaction

--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -4,48 +4,35 @@ import * as d3 from 'd3';
 
 export default class StockChart extends React.Component {
   render() {
-    let startingDate;
-    let stockMaximums;
-    let stockMinimums;
-    let stockMin;
+    const div = new ReactFauxDOM.Element('div');
     let stockMax;
-    let largestDataSet = { closingValues: [] };
     const rawData = this.props.data;
+    const smallestDataSetSize = d3.min(rawData.map((stock) => stock.closingValues.length));
+    const restrictingDataSet = rawData.filter((stock) => stock.closingValues.length === smallestDataSetSize)[0];
     const stockLines = [];
+    const parseTime = d3.timeParse('%Y-%m-%d');
     const margin = {top: 20, right: 40, bottom: 70, left: 40}
     const width = window.innerWidth - margin.left - margin.right;
     const height = window.innerHeight - margin.top - margin.bottom - 75*2; // -75 for header and for bottom stock info
-    const parseTime = d3.timeParse('%d-%b-%y');
     const x = d3.scaleTime().range([0, width]);
     const y = d3.scaleLinear().range([height, 0]);
-    const xAxis = d3.axisBottom(x).ticks(8).tickFormat(d3.timeFormat('%b %d, %Y'));
-    const yAxis = d3.axisLeft().scale(y).ticks(5);
-    const div = new ReactFauxDOM.Element('div');
+    const xAxis = d3.axisBottom(x).ticks(10).tickFormat(d3.timeFormat('%b %d, %Y'));
+    const yAxis = d3.axisLeft(y).ticks(5);
 
     // Get prelimiary data to set up chart
-    rawData.map((d) => {
-      if (d.closingValues.length > largestDataSet.closingValues.length) {
-        startingDate = d.closingValues[d.closingValues.length - 1].date;
-        largestDataSet = d;
-      }
-    });
-    stockMaximums = rawData.map((stock) => d3.max(stock.closingValues.map((d) => parseInt(d.price))));
-    stockMinimums = rawData.map((stock) => d3.min(stock.closingValues.map((d) => parseInt(d.price))));
-    stockMax = d3.max(stockMaximums);
-    stockMin = d3.min(stockMinimums);
+    // fix these to only use valid dates
+    stockMax = d3.max(rawData.map((stock) => d3.max(stock.closingValues.map((d) => parseInt(d.price)))));
     
     // Fromat data into a JSON format that can be used with D3
     const data = { stocks: []};
-    for (let i = largestDataSet.closingValues.length - 1; i > -1; i--) {
-      const date = largestDataSet.closingValues[i].date;
+    for (let i = smallestDataSetSize - 1; i > -1; i--) {
+      const date = restrictingDataSet.closingValues[i].date;
       data.stocks.push({
         'date': date
       });
       rawData.map((stock) => {
         const stockData = stock.closingValues.filter((stockData) => stockData.date === date);
-        if (stockData.length > 0) {
-          data.stocks[data.stocks.length - 1][stock.name] = stockData[0].price;
-        }
+        data.stocks[data.stocks.length - 1][stock.name] = parseFloat(stockData[0].price);
       });
     }
 
@@ -56,34 +43,48 @@ export default class StockChart extends React.Component {
       .append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    x.domain([new Date(startingDate),new Date()]);
-    y.domain([stockMin*9/10, stockMax*11/10]);
+    x.domain(d3.extent(data.stocks, (d) => parseTime(d.date)));
+    y.domain([0, stockMax*11/10]);
 
     svg.append('g')
       .attr('transform', `translate(0, ${height})`)
       .call(xAxis)
       .selectAll('text')
         .style('text-anchor', 'end')
-        .attr('dx', '-0.8rem')
-        .attr('dy', '.15rem')
+        .attr('dx', '-1rem')
+        .attr('dy', '-.2rem')
         .attr('transform', 'rotate(-65)');
     
     svg.append('g')
       .call(yAxis);
 
+    const valueline = d3.line()
+      .x((d) => x(d.date))
+      .y((d) => y(d['GOOG']));
+
+    data.stocks.forEach((d) => {
+      d.date = parseTime(d.date);
+      d['GOOG'] = +d['GOOG'];
+    });
+
     rawData.map((stock) => {
       data.stocks.map((stockData) => {
         const stockLine = d3.line()
-          .x((d) => x(d.Date))
-          .y((d) => y(d[stock]));
+          .x((d) => x(d.date))
+          .y((d) => y(d[stock.name]));
         stockLines.push(stockLine);
       })
     });
 
+    const colours = ['steelblue', 'red', 'green', 'blue', 'pink', 'yellow'];
+
     stockLines.forEach((stockLine) => {
+      let stockColour;
+        stockColour = colours[stockLines.indexOf(stockLine)%6];
       svg.append('path')
-        .data([data])
-        .style('stroke', 'red')
+        .datum(data.stocks)
+        .attr('class', 'line')
+        .attr('stroke', stockColour)
         .attr('d', stockLine);
     });
 
