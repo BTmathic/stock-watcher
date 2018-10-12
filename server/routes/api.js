06966f2ff1face482a249611bbd967a3af594a24
Alexander Molnar <mathic@gmail.com>
2018-10-11 21:58:06 -0400

    Naive approach to keeping stock data up to date (in the database) added in the back end, to get around the (free API) 5 calls/minute max preventing a user from loading data quickly

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const admin = require('firebase-admin');
 const serviceAccount = require('../../stock-watcher-fb-adminsdk.json');
 admin.initializeApp({
@@ -8,14 +9,39 @@ admin.initializeApp({
 
 const db = admin.database();
 const ref = db.ref('/stocks');
+let snapshotIndex = 1;
 
 module.exports = (app) => {
-
+  
   setInterval(() => {
+    const currentDate = new Date().getUTCDate();
     ref.once('value', (snapshot) => {
       snapshot.forEach((childSnapshot) => {
-        console.log(childSnapshot.val());
+        const stock = childSnapshot.val();
+        const date = new Date(stock.lastUpdated).getUTCDate();
+        snapshotIndex++;
+        setTimeout(() => {
+          if (date !== currentDate) {
+            fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${stock.name}&apikey=${process.env.API_KEY}`)
+              .then((resp) => resp.json())
+              .then((json) => {
+                const metaData = json['Meta Data'];
+                const values = json['Weekly Time Series'];
+                const lastUpdated = metaData['3. Last Refreshed'].split(' ')[0];
+                const closingValues = Object.keys(values).map((date) => ({
+                  date,
+                  price: values[date]['4. close']
+                }));
+                ref.child(childSnapshot.key).set({
+                  name: stock.name,
+                  lastUpdated,
+                  closingValues
+                });
+              });
+          }
+        }, 15000 * snapshotIndex);
       });
     });
-  }, 1000);
+  }, 2000);
+
 };
