e64dafb5d65bb68be41b06762b447f28aa7e0cc2
Alexander Molnar <mathic@gmail.com>
2018-12-17 12:13:49 -0500

    Alternate fix for JSON being overstringified

--- server/server.js
+++ server/server.js
@@ -15,13 +15,13 @@ const serviceAccount = {
   type: process.env.FIREBASE_SDK_TYPE,
   project_id: process.env.FIREBASE_SDK_PROJECT_ID,
   private_key_id: process.env.FIREBASE_SDK_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_SDK_PRIVATE_KEY,
-  "client_email": process.env.FIREBASE_SDK_CLIENT_EMAIL,
-  "client_id": process.env.FIREBASE_SDK_CLIENT_ID,
-  "auth_uri": process.env.FIREBASE_SDK_AUTH_URI,
-  "token_uri": process.env.FIREBASE_SDK_TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.FIREBASE_SDK_AUTH_PROVIDER,
-  "client_x509_cert_url": process.env.FIREBASE_SDK_CLIENT_CERT
+  private_key: process.env.FIREBASE_SDK_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_SDK_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_SDK_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_SDK_AUTH_URI,
+  token_uri: process.env.FIREBASE_SDK_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_SDK_AUTH_PROVIDER,
+  client_x509_cert_url: process.env.FIREBASE_SDK_CLIENT_CERT
 }
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
